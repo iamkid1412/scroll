49b516764343601b22018c20a97256f46b868c07
Roger Huang <626369927@qq.com>
2011-10-19 16:19:09 +0800

    Add direction function.

--- scroll.js
+++ scroll.js
@@ -12,18 +12,42 @@ jQuery.extend({
 	  * @param slideClass: the class name of the slide, it's defined by user.
 	  * @param intervalTime: the interval time.
 	  * @param animTime: the animate time.
+	  * @param isTop: if true, scroll vertically, otherwise, scroll horizontally.
 	  */
-	imgScroll: function (elem, slideClass, intervalTime, animTime) {
+	imgScroll: function (elem, slideClass, intervalTime, animTime, isTop) {
 		var win = window,
 			imgBox = $(elem),
-			width = imgBox.find("img").width(),
+			img = imgBox.find("img"),
+			width = $(win).width(),
+			height = img.height(),
 			currIndex = 0,
-			count = imgBox.find("img").length,
+			count = img.length,
 			timer = 0,
+			direction = "",
+			distance = 0,
+			styles = {},
 			start,
 			stop,
+			initImages,
+			initDirection,
 			initSlide,
 			bindEvent;
+
+		initImages = function () {
+			imgBox.find("img").css("width", width);
+		};
+		initDirection = function () {
+			if (isTop) {
+				imgBox.addClass("isTop");
+				direction = "top";
+				distance = height;
+				styles = {"top": "-=" + distance};
+			} else {
+				direction = "left";
+				distance = width;
+				styles = {"left": "-=" + distance};
+			}
+		};
 		/**
 		 * Initialize the slide box
 		 * Including create the slide box and append it after the images box.
@@ -35,7 +59,6 @@ jQuery.extend({
 				slideBox,
 				slideList;
 
-			imgBox.find(":first-child").clone().appendTo(imgBox); // Add the first images after the images list for the animation, any better ways?
 			template.push("<ul class='" + slideClass + "'>");
 			for (i = 0; i < count; i = i + 1) {
 				if (i) {
@@ -66,7 +89,7 @@ jQuery.extend({
 					currIndex = index;
 					$(slideList[currIndex]).addClass("active");
 					imgBox.stop();
-					imgBox.css("left", -currIndex * width);
+					imgBox.css(direction, -currIndex * distance);
 				});
 			});
 			imgBox.delegate("img", "mouseover", function () {
@@ -81,20 +104,23 @@ jQuery.extend({
 		 * You can use the function to start the timer outside.
 		 */
 		start = function () {
-			var slideList = $("." + slideClass + " li");
+			var slideList = $("." + slideClass + " li"),
+				firstImg = imgBox.find("img").first();
 
 			timer = win.setInterval(function () {
 				if (currIndex !== count - 1) {
 					$(slideList[currIndex]).removeClass("active");
 					currIndex = currIndex + 1;
 					$(slideList[currIndex]).addClass("active");
-					imgBox.animate({"left": "-=" + width}, animTime);
+					imgBox.animate(styles, animTime);
 				} else {
 					currIndex = 0;
 					$(slideList[count - 1]).removeClass("active");
 					$(slideList[currIndex]).addClass("active");
-					imgBox.animate({"left": "-=" + width}, animTime, function () {
-						imgBox.css("left", 0);
+					firstImg.css(direction, count * distance);
+					imgBox.animate(styles, animTime, function () {
+						imgBox.css(direction, 0);
+						firstImg.css(direction, 0);
 					});
 				}
 			}, intervalTime);
@@ -112,6 +138,8 @@ jQuery.extend({
 			 * Including initialize the slide box, initialize the timer and band events.
 			 */
 			enable: function () {
+				initImages();
+				initDirection();
 				initSlide();
 				start();
 				bindEvent();
